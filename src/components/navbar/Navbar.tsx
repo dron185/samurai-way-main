8c57bd298d16324162d453838934c7108736443e
Andrei Huryn <gurin.nord@gmail.com>
2024-03-09 11:37:33 +0300

    fix: add list and change div to li

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,11 +6,13 @@ import {Container} from "../container/Container";
 export const Navbar = () => {
     return (
         <nav className={s.nav}>
-                <div className={`${s.item} ${s.active}`}><NavLink to={"/profile"} activeClassName={s.activeLink}>Profile</NavLink></div>
-                <div className={s.item}><NavLink to={"/dialogs"} activeClassName={s.activeLink}>Messages</NavLink></div>
-                <div className={s.item}><NavLink to={"/news"} activeClassName={s.activeLink}>News</NavLink></div>
-                <div className={s.item}><NavLink to={"/music"} activeClassName={s.activeLink}>Music</NavLink></div>
-                <div className={s.item}><NavLink to={"/settings"} activeClassName={s.activeLink}>Settings</NavLink></div>
+            <ul>
+                <li className={`${s.item} ${s.active}`}><NavLink to={"/profile"} activeClassName={s.activeLink}>Profile</NavLink></li>
+                <li className={s.item}><NavLink to={"/dialogs"} activeClassName={s.activeLink}>Messages</NavLink></li>
+                <li className={s.item}><NavLink to={"/news"} activeClassName={s.activeLink}>News</NavLink></li>
+                <li className={s.item}><NavLink to={"/music"} activeClassName={s.activeLink}>Music</NavLink></li>
+                <li className={s.item}><NavLink to={"/settings"} activeClassName={s.activeLink}>Settings</NavLink></li>
+            </ul>
         </nav>
     );
 };
