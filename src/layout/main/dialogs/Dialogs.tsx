46b028c1b9db5c22eb0ffe2e22471532fb680cc5
Andrei Huryn <gurin.nord@gmail.com>
2024-04-12 17:45:51 +0300

    feat: add textarea and button to dialogs

--- src/layout/main/dialogs/Dialogs.tsx
+++ src/layout/main/dialogs/Dialogs.tsx
@@ -16,6 +16,12 @@ export const Dialogs: React.FC<DialogsPropsType> = (props) => {
     let messagesElements = props.messages.map(el =><div className={s.messageWrapper}><img className={s.avatar} src={avatar} alt="avatar"/><Message
         key={el.id} message={el.message}/></div>)
 
+    const newTextElement = React.createRef<HTMLTextAreaElement>();
+    const addMessage = () => {
+        let text = newTextElement.current?.value;
+        alert(text);
+    }
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
@@ -23,6 +29,15 @@ export const Dialogs: React.FC<DialogsPropsType> = (props) => {
             </div>
             <div className={s.messages}>
                 {messagesElements}
+                <div>
+                    <div>
+                        <textarea ref={newTextElement}></textarea>
+                    </div>
+                    <div>
+                        <button onClick={addMessage}>Add message</button>
+                    </div>
+                    {/*<button>Remove</button>*/}
+                </div>
             </div>
         </div>
     );
