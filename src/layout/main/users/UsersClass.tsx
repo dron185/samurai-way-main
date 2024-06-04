020c00ff3aa2a96cc8033542a0c57ec05c53bd5f
Andrei Huryn <gurin.nord@gmail.com>
2024-06-04 11:18:39 +0300

    feat: add componentDidMount to UsersClass

--- src/layout/main/users/UsersClass.tsx
+++ src/layout/main/users/UsersClass.tsx
@@ -5,9 +5,11 @@ import axios from "axios";
 import {UserType} from "../../../redux/users-reducer";
 
 export class UsersClass extends React.Component<any, any> {
-    constructor(props: any) {
-        super(props);
+    // constructor(props: any) {
+    //     super(props);
+    // }
 
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
             this.props.setUsers(response.data.items)
         });
