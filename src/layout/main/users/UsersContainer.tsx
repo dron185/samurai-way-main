71e320c8fe5ed1d4d9feaa022da82feec028aa35
Andrei Huryn <gurin.nord@gmail.com>
2024-06-06 12:36:35 +0300

    feat: moved UsersAPIComponent to UsersContainer.tsx

--- src/layout/main/users/UsersContainer.tsx
+++ src/layout/main/users/UsersContainer.tsx
@@ -9,7 +9,37 @@ import {
     UsersPageType,
     UserType
 } from "../../../redux/users-reducer";
-import {UsersAPIComponent} from "./UsersAPIComponent";
+import React from "react";
+import axios from "axios";
+import {UsersFC} from "./UsersFC";
+
+export class UsersAPIComponent extends React.Component<UsersContainerPropsType> {
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items);
+            this.props.setTotalUsersCount(response.data.totalCount);
+        });
+    }
+
+    onPageChanged = (pageNumber: number) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items)
+        });
+    }
+
+    render() {
+        return <UsersFC
+            totalUsersCount={this.props.totalUsersCount}
+            pageSize={this.props.pageSize}
+            currentPage={this.props.currentPage}
+            onPageChanged={this.onPageChanged}
+            users={this.props.users}
+            follow={this.props.follow}
+            unfollow={this.props.unfollow}
+        />
+    }
+}
 
 type MapDispatchToPropsType = {
     follow: (userId: number) => void
