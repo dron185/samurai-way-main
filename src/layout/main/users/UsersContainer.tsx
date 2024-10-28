330d56a9210d1c06218abbc4e233099ce47492ae
Andrei Huryn <gurin.nord@gmail.com>
2024-10-28 12:55:48 +0300

    feat: remove unused props from UsersContainer

--- src/layout/main/users/UsersContainer.tsx
+++ src/layout/main/users/UsersContainer.tsx
@@ -4,24 +4,19 @@ import {
     followAC,
     getUsersThunkCreatorTC,
     setCurrentPageAC,
-    setUsersAC,
-    setUsersTotalCountAC,
     toggleFollowingProgressAC,
-    toggleIsFetchingAC,
     unfollowAC,
-    UsersPageType,
-    UserType
+    UsersPageType
 } from "../../../redux/users-reducer";
 import React from "react";
 import {UsersFC} from "./UsersFC";
 import {Preloader} from "../../../components/preloader/Preloader";
-import {usersAPI} from "../../../api/api";
 
 class UsersContainer extends React.Component<UsersContainerPropsType> {
     // все сайд-эффекты делаются в методе жизненного цикла - componentDidMount():
     componentDidMount() {
 
-        this.props.getUsersThunkCreator(this.props.currentPage, this.props.pageSize)
+        this.props.getUsers(this.props.currentPage, this.props.pageSize)
         // this.props.toggleIsFetching(true); // - когда идет запрос на сервак
         //
         // usersAPI.getUsers(this.props.currentPage, this.props.pageSize)
@@ -33,14 +28,18 @@ class UsersContainer extends React.Component<UsersContainerPropsType> {
     }
 
     onPageChanged = (pageNumber: number) => {
-        this.props.setCurrentPage(pageNumber);
-        this.props.toggleIsFetching(true); // - когда меняем страницу
 
-        usersAPI.getUsers(pageNumber, this.props.pageSize)
-            .then(data => {
-                this.props.toggleIsFetching(false); // - когда приходит ответ
-                this.props.setUsers(data.items)
-            });
+        this.props.getUsers(pageNumber, this.props.pageSize)
+
+        this.props.setCurrentPage(pageNumber);
+
+        // this.props.toggleIsFetching(true); // - когда меняем страницу
+        //
+        // usersAPI.getUsers(pageNumber, this.props.pageSize)
+        //     .then(data => {
+        //         this.props.toggleIsFetching(false); // - когда приходит ответ
+        //         this.props.setUsers(data.items)
+        //     });
     }
 
     render() {
@@ -65,12 +64,9 @@ class UsersContainer extends React.Component<UsersContainerPropsType> {
 type MapDispatchToPropsType = {
     follow: (userId: number) => void
     unfollow: (userId: number) => void
-    setUsers: (users: UserType[]) => void
     setCurrentPage: (pageNumber: number) => void
-    setTotalUsersCount: (totalUsersCount: number) => void
-    toggleIsFetching: (isFetching: boolean) => void
     toggleFollowingProgress: (isFetching: boolean, userId: number) => void
-    getUsersThunkCreator: (currentPage: number, pageSize: number) => void
+    getUsers: (currentPage: number, pageSize: number) => void
 }
 
 export type UsersContainerPropsType = UsersPageType & MapDispatchToPropsType
@@ -94,22 +90,13 @@ const mapDispatchToProps = (dispatch: AppDispatch): MapDispatchToPropsType => {
         unfollow: (userId: number) => {
             dispatch(unfollowAC(userId))
         },
-        setUsers: (users: UserType[]) => {
-            dispatch(setUsersAC(users))
-        },
         setCurrentPage: (pageNumber: number) => {
             dispatch(setCurrentPageAC(pageNumber))
-        },
-        setTotalUsersCount: (totalCount: number) => {
-            dispatch(setUsersTotalCountAC(totalCount))
-        },
-        toggleIsFetching: (isFetching: boolean) => {
-            dispatch(toggleIsFetchingAC(isFetching))
         },
         toggleFollowingProgress: (isFetching: boolean, userId: number) => {
             dispatch(toggleFollowingProgressAC(isFetching, userId))
         },
-        getUsersThunkCreator: (currentPage: number, pageSize: number) => {
+        getUsers: (currentPage: number, pageSize: number) => {
             dispatch(getUsersThunkCreatorTC(currentPage, pageSize))
         }
     }
@@ -118,10 +105,7 @@ const mapDispatchToProps = (dispatch: AppDispatch): MapDispatchToPropsType => {
 export default connect(mapStateToProps, /*{
     follow: followAC,
     unfollow: unfollowAC,
-    setUsers: setUsersAC,
     setCurrentPage: setCurrentPageAC,
-    setTotalUsersCount: setUsersTotalCountAC,
-    toggleIsFetching: toggleIsFetchingAC,
     toggleFollowingProgress: toggleFollowingProgressAC
 }*/ mapDispatchToProps )(UsersContainer)
 
