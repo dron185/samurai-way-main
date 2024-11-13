fbfb185e1668d97ab264ec314355aea826d08bf6
Andrei Huryn <gurin.nord@gmail.com>
2024-11-13 18:02:29 +0300

    chore: replace BrowserRouter with HashRouter for gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {store} from './redux/redux-store';
 import {Provider} from "react-redux";
 
 ReactDOM.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
         <Provider store={store}>
             <App/>
         </Provider>
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById('root')
 );
 
